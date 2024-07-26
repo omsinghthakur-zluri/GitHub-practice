cb6374097940b6ad20b75b49b0cbd70d7a1b6e83
Coding2alpha <omsinghthakur2502@gmail.com>
2024-07-26 15:59:46 +0530

    hello from main

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 // var message = "Hello, world!";
 app.use(cors());
-app.use(express.json());  
+app.use(express.json());
 
 app.use("/api/transactions", transactionRouter);
 
@@ -20,3 +20,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+console.log("hello from main");
